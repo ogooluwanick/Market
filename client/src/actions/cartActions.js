a0a906e7d75d7a89acebad59e48f9a513ee642a9
Ogooluwa Olutimilehin <ogooluwanick@gmail.com>
2022-06-20 00:26:16 +0300

    production

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -3,7 +3,7 @@ import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SH
 
 
 export const addToCart=(id,qty)=>async(dispatch,getState)=>{
-        const {data}= await axios.get(`/api/${id}`)
+        const {data}= await axios.get(`/api/products/${id}`)
        
         dispatch({type:CART_ADD_ITEM, payload:{
                 product:data._id,
