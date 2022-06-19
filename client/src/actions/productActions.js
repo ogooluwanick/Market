5e20824c1e006efd80e693a6dde5cc6d783f78aa
Ogooluwa Olutimilehin <ogooluwanick@gmail.com>
2022-06-19 23:50:52 +0300

    set

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,13 +1,13 @@
 import  axios  from "axios";
 import { CREATE_PRODUCT_FAIL, CREATE_PRODUCT_REQUEST, CREATE_PRODUCT_SUCCESS, DELETE_PRODUCT_FAIL, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_REVIEW_FAIL, PRODUCT_REVIEW_REQUEST, PRODUCT_REVIEW_SUCCESS, PRODUCT_TOP_CAROUSEL_FAIL, PRODUCT_TOP_CAROUSEL_REQUEST, PRODUCT_TOP_CAROUSEL_SUCCESS, UPDATE_PRODUCT_FAIL, UPDATE_PRODUCT_REQUEST, UPDATE_PRODUCT_SUCCESS } from "../constants/constants";
 
-const baseURL="marketapphog.herokuapp.com/api"
+// const baseURL="marketapphog.herokuapp.com/api"
 
 export const listProducts =(keyword="",pageNumber)=> async(dispatch) =>{
         dispatch({type:PRODUCT_LIST_REQUEST})
 
         try {
-                const {data}= await axios.get(`${baseURL}/products?keyword=${keyword}&pageNumber=${pageNumber}`)
+                const {data}= await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
                 dispatch({type:PRODUCT_LIST_SUCCESS, payload: data})
         } catch (error) {
                 dispatch({
@@ -23,7 +23,7 @@ export const listTopCarousel =()=> async(dispatch) =>{
         dispatch({type:PRODUCT_TOP_CAROUSEL_REQUEST})
 
         try {
-                const {data}= await axios.get(`${baseURL}/products/top`)
+                const {data}= await axios.get(`/api/products/top`)
                 dispatch({type:PRODUCT_TOP_CAROUSEL_SUCCESS, payload: data})
         } catch (error) {
                 dispatch({
@@ -42,7 +42,7 @@ export const detailsProduct =(productsID)=> async (dispatch) => {
         });
     
         try{
-            const {data} = await axios.get(`${baseURL}/products/${productsID}`);
+            const {data} = await axios.get(`/api/products/${productsID}`);
             dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
         }
         catch(error){
@@ -63,7 +63,7 @@ export const reviewProduct =(productsID,review)=> async (dispatch,getState) => {
         try{
                 const config= {headers:{Authorization:`Bearer ${userInfo.token}`}}      
 
-            await axios.post(`${baseURL}/products/${productsID}/reviews`,review,config);
+            await axios.post(`/api/products/${productsID}/reviews`,review,config);
             dispatch({type: PRODUCT_REVIEW_SUCCESS});
         }
         catch(error){
@@ -83,7 +83,7 @@ export const adminDeleteProduct =(productsID)=> async (dispatch,getState) => {
         try{
                 const config= {headers:{Authorization:`Bearer ${userInfo.token}`}}      
 
-             await axios.delete(`${baseURL}/products/${productsID}`,config);
+             await axios.delete(`/api/products/${productsID}`,config);
             dispatch({type: DELETE_PRODUCT_SUCCESS});
         }
         catch(error){
@@ -103,7 +103,7 @@ export const adminDeleteProduct =(productsID)=> async (dispatch,getState) => {
         try{
                 const config= {headers:{Authorization:`Bearer ${userInfo.token}`}}      
 
-                const {data}= await axios.post(`${baseURL}/products/`,{},config);
+                const {data}= await axios.post(`/api/products/`,{},config);
                 dispatch({type: CREATE_PRODUCT_SUCCESS,payload: data});
                 nav(`/admin/product/${data._id}/edit`)
         }
@@ -124,7 +124,7 @@ export const adminDeleteProduct =(productsID)=> async (dispatch,getState) => {
         try{
                 const config= {headers:{Authorization:`Bearer ${userInfo.token}`}}      
 
-                const {data}= await axios.put(`${baseURL}/products/${productId}`,product,config);
+                const {data}= await axios.put(`/api/products/${productId}`,product,config);
             dispatch({type: UPDATE_PRODUCT_SUCCESS,payload: data});
         }
         catch(error){
